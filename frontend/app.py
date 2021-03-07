3529d9f3c01314249668cfa9e3afb8b738df67ef
Chris Mague <github@mague.com>
2021-03-07 13:05:21 -0800

    working demo with message flows and retries

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
-from redisearch import Client, Query, aggregation, reducers, IndexDefinition, TextField, NumericField, TagField
+from redisearch import Client, Query, aggregation, reducers, IndexDefinition, TextField, NumericField, TagField, NumericFilter
 
 
 # From our local file
@@ -12,6 +12,8 @@ from os import environ, getcwd
 
 import redis
 import yaml
+import re
+import datetime
 
 app = Flask(__name__,
             static_url_path='/docs',
@@ -42,6 +44,7 @@ client = Client(
 nav = Nav()
 topbar = Navbar('',
     View('Home', 'index'),
+    View('Retries', 'show_retries'),
 )
 nav.register_element('top', topbar)
 
@@ -52,6 +55,33 @@ def index():
    except redis.exceptions.ResponseError:
        setup_data(cfg)
    return render_template('top.html', microservices = cfg['microservices'])
+
+@app.route('/messages')
+def show_message():
+   messageid = request.args.get('message')
+   j = client.search(Query(messageid).limit_fields('id').verbatim()).docs[0].__dict__
+   print(j)
+   del j['id']
+   del j['payload']
+   p = re.compile(r'(\d{13,14})\-\d{1,4}')
+
+   for x in j:
+      m = p.search(j[x])
+      if m:
+         j[x] = datetime.datetime.fromtimestamp(float(m.group(1))/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
+
+
+   return render_template('showmessage.html', messages = j, messageid = messageid)
+
+@app.route('/retry')
+def show_retries():
+  res = []
+  for ms in cfg['microservices']:
+     j = client.search(Query('*').add_filter(NumericFilter('%s_RETRY' % ms['name'], 1, NumericFilter.INF, minExclusive=False))).docs
+     for w in list(map(lambda x : x.id, j)):
+        res.append(w.replace('STATE:', ''))
+  return render_template('showretries.html', messages = res)
+
 
 if __name__ == '__main__':
    bootstrap.init_app(app)
