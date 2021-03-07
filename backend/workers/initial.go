ef0477b36ce3787fa8c4f5a967656658c70c90f5
Chris Mague <github@mague.com>
2021-03-07 11:58:57 -0800

    make the id non-tokenizable

--- backend/workers/initial.go
+++ backend/workers/initial.go
@@ -19,12 +19,12 @@ func InitialWorker(ms types.Microservice, redisClient *redis.Client, ctx context
 		"messages": "10000",
 		"prefix":   "message",
 	}
-	redisClient.XGroupCreateMkStream(ctx, ms.Input, fmt.Sprintf("Group-%s", ms.Input), "0").Err()
+	redisClient.XGroupCreateMkStream(ctx, ms.Name, fmt.Sprintf("Group-%s", ms.Name), "0").Err()
 	for {
 		res, _ := redisClient.XReadGroup(ctx, &redis.XReadGroupArgs{
-			Group:    fmt.Sprintf("Group-%s", ms.Input),
-			Consumer: fmt.Sprintf("Consumer-%s", ms.Input),
-			Streams:  []string{ms.Input, ">"},
+			Group:    fmt.Sprintf("Group-%s", ms.Name),
+			Consumer: fmt.Sprintf("Consumer-%s", ms.Name),
+			Streams:  []string{ms.Name, ">"},
 			Count:    1,
 			Block:    1 * time.Second,
 		}).Result()
@@ -34,9 +34,9 @@ func InitialWorker(ms types.Microservice, redisClient *redis.Client, ctx context
 				for key, element := range y.Values {
 					runVars[key] = element.(string)
 				}
-				_, errack := redisClient.XAck(ctx, ms.Input, fmt.Sprintf("Group-%s", ms.Input), y.ID).Result()
+				_, errack := redisClient.XAck(ctx, ms.Name, fmt.Sprintf("Group-%s", ms.Name), y.ID).Result()
 				if errack != nil {
-					log.Printf("%s: Unable to ack message: %s %s ", ms.Input, y.ID, errack)
+					log.Printf("%s: Unable to ack message: %s %s ", ms.Name, y.ID, errack)
 				}
 			}
 			p, err := strconv.Atoi(runVars["messages"])
@@ -46,7 +46,7 @@ func InitialWorker(ms types.Microservice, redisClient *redis.Client, ctx context
 			}
 
 			for z := 0; z < msgCount; z++ {
-				myname := fmt.Sprintf("%s-%d", runVars["prefix"], z)
+				myname := fmt.Sprintf("%s%09d", runVars["prefix"], z)
 				redisClient.XAdd(ctx, &redis.XAddArgs{
 					Stream: ms.Output,
 					ID:     "*",
